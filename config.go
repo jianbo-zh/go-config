c84db6401ac3b4c69b15191a9837bbac77382a71
jianbo-zh <tipine@163.com>
2023-04-23 14:27:10 +0800

    增加一些获取方法

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/spf13/cast"
 )
 
@@ -52,6 +54,10 @@ func GetInt(path string, defaultValue ...interface{}) int {
 	return cast.ToInt(Get(path, defaultValue...))
 }
 
+func GetInt32(key string, defaultValue ...interface{}) int32 {
+	return cast.ToInt32(Get(key, defaultValue...))
+}
+
 // GetInt64 获取 Int64 类型的配置信息
 func GetInt64(path string, defaultValue ...interface{}) int64 {
 	return cast.ToInt64(Get(path, defaultValue...))
@@ -62,7 +68,49 @@ func GetUint(path string, defaultValue ...interface{}) uint {
 	return cast.ToUint(Get(path, defaultValue...))
 }
 
-// GetBool 获取 Bool 类型的配置信息
+func GetUint16(key string, defaultValue ...interface{}) uint16 {
+	return cast.ToUint16(Get(key, defaultValue...))
+}
+
+func GetUint32(key string, defaultValue ...interface{}) uint32 {
+	return cast.ToUint32(Get(key, defaultValue...))
+}
+func GetUint64(key string, defaultValue ...interface{}) uint64 {
+	return cast.ToUint64(Get(key, defaultValue...))
+}
+
 func GetBool(path string, defaultValue ...interface{}) bool {
 	return cast.ToBool(Get(path, defaultValue...))
+}
+
+func GetFloat64(key string, defaultValue ...interface{}) float64 {
+	return cast.ToFloat64(Get(key, defaultValue...))
+}
+
+func GetTime(key string, defaultValue ...interface{}) time.Time {
+	return cast.ToTime(Get(key, defaultValue...))
+}
+
+func GetDuration(key string, defaultValue ...interface{}) time.Duration {
+	return cast.ToDuration(Get(key, defaultValue...))
+}
+
+func GetIntSlice(key string, defaultValue ...interface{}) []int {
+	return cast.ToIntSlice(Get(key, defaultValue...))
+}
+
+func GetStringSlice(key string, defaultValue ...interface{}) []string {
+	return cast.ToStringSlice(Get(key, defaultValue...))
+}
+
+func GetStringMap(key string, defaultValue ...interface{}) map[string]interface{} {
+	return cast.ToStringMap(Get(key, defaultValue...))
+}
+
+func GetStringMapString(key string, defaultValue ...interface{}) map[string]string {
+	return cast.ToStringMapString(Get(key, defaultValue...))
+}
+
+func GetStringMapStringSlice(key string, defaultValue ...interface{}) map[string][]string {
+	return cast.ToStringMapStringSlice(Get(key, defaultValue...))
 }
